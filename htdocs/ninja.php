c327081c9495b2fbdc98b6f6ce0f67805e4ccd80
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-06-27 14:48:05 +0900

    PHP 3-2

--- htdocs/ninja.php
+++ htdocs/ninja.php
@@ -5,7 +5,15 @@
 <title>送受信</title>
 </head>
 <body>
+<form>
+  <input type="text" name="a">
+  <input type="submit" value="送信するよ！">
+</form>
 
-
+<?php
+$mozi=htmlspecialchars($_GET["a"], ENT_QUOTES);
+print isset($_GET["a"])?$mozi."でござる":"何かしゃべって！";
+?>
+<img src="nin.gif">
 </body>
 </html>
